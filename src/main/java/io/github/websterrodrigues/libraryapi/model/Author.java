8f7f459eb046812b956149edc845efac6920fa80
Webster Diogenes <web.driw12@gmail.com>
2025-05-13 21:38:07 -0300

    Author mapping

---
+++ src/main/java/io/github/websterrodrigues/libraryapi/model/Author.java
@@ -0,0 +1,44 @@
+package io.github.websterrodrigues.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity //Mapeamento JPA
+@Table(name = "autor")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "nacionalidade", nullable = false, length = 50)
+    private String nationality;
+
+    @Deprecated //Impede que o construtor padrão seja utilizado
+    public Author(){
+        //Para uso do framework
+
+    }
+
+    public Author(UUID id, String name, String nationality, LocalDate dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+}
