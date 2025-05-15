07e89626bf0a6c6a03b670aff05390b5e9b205e8
Webster Diogenes <web.driw12@gmail.com>
2025-05-15 12:17:57 -0300

    Persistence with repository

--- src/main/java/io/github/websterrodrigues/libraryapi/Application.java
+++ src/main/java/io/github/websterrodrigues/libraryapi/Application.java
@@ -1,13 +1,28 @@
 package io.github.websterrodrigues.libraryapi;
 
+import io.github.websterrodrigues.libraryapi.model.Author;
+import io.github.websterrodrigues.libraryapi.repository.AuthorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		var context = SpringApplication.run(Application.class, args);
+		AuthorRepository repository = context.getBean(AuthorRepository.class);
+		examplesSaveRegister(repository);
 	}
 
+	public static void examplesSaveRegister(AuthorRepository repository){
+		Author author = new Author();
+		author.setName("AUTORTESTE");
+		author.setNationality("BRASILEIRO");
+		author.setDateOfBirth(LocalDate.of(1990, 1, 1));
+
+		repository.save(author);
+
+	}
 }
