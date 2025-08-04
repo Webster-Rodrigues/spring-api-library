89c6a3aaac543f697d9b3994d2e062702bb0a74b
Webster Rodrigues <web.driw12@gmail.com>
2025-08-03 21:12:15 -0300

    add CustomRegisteredClientRepository

---
+++ src/main/java/io/github/websterrodrigues/libraryapi/security/CustomRegisteredClientRepository.java
@@ -0,0 +1,45 @@
+package io.github.websterrodrigues.libraryapi.security;
+
+import io.github.websterrodrigues.libraryapi.model.Client;
+import io.github.websterrodrigues.libraryapi.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    @Autowired
+    private ClientService service;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        Client client = service.findByClientId(clientId);
+
+        if(client == null) {
+            return null;
+        }
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+    }
+}
