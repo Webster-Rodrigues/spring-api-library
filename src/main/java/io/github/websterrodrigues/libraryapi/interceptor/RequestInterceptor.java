800e48cc15022a154e8140bd57a3bb3f253c6277
Webster Rodrigues <web.driw12@gmail.com>
2025-08-30 22:12:28 -0300

    configure logs in the interceptor

--- src/main/java/io/github/websterrodrigues/libraryapi/interceptor/RequestInterceptor.java
+++ src/main/java/io/github/websterrodrigues/libraryapi/interceptor/RequestInterceptor.java
@@ -2,6 +2,8 @@ package io.github.websterrodrigues.libraryapi.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,22 +11,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class RequestInterceptor implements HandlerInterceptor {
 
+    private static final Logger logger = LoggerFactory.getLogger(RequestInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
-
-        System.out.println("Interceptando requisição: " + request.getRequestURI());
+        logger.info("Iniciando requisição: [{}] {}", request.getMethod(), request.getRequestURI());
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("Após o controlador: " + request.getRequestURI());
+        logger.debug("Controller executadocom sucesso {}", request.getRequestURI());
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception)  throws Exception {
-        System.out.println("Requisição concluída: " + request.getRequestURI());
+        logger.info("[{}] {} | Requisição finalizada | Status Code: {}", request.getMethod(), request.getRequestURI(), response.getStatus());
     }
-
-
 }
