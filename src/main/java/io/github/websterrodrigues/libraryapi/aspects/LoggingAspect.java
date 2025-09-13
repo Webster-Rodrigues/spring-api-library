f539b35eec52e12011dc22b12efe995db3ed9c02
Webster Diogenes <web.driw12@gmail.com>
2025-09-12 23:32:56 -0300

    creates AuthorService pointcut and deletes log file

--- src/main/java/io/github/websterrodrigues/libraryapi/aspects/LoggingAspect.java
+++ src/main/java/io/github/websterrodrigues/libraryapi/aspects/LoggingAspect.java
@@ -1,24 +1,39 @@
 package io.github.websterrodrigues.libraryapi.aspects;
 
 
-import org.aspectj.lang.JoinPoint;
+import io.github.websterrodrigues.libraryapi.model.SystemUser;
+import io.github.websterrodrigues.libraryapi.security.SecurityService;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Around("execution(* io.github.websterrodrigues.libraryapi.service.AuthorService.*(..))")
+    @Autowired
+    private SecurityService securityService;
+
+    private final static Logger logger = LoggerFactory.getLogger(LoggingAspect .class);
+
+    @Pointcut("execution(* io.github.websterrodrigues.libraryapi.service.AuthorService.*(..))")
+    public void authorServiceMethods(){}
+
+    @Around("authorServiceMethods()")
     public Object LogAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("Antes [TESTE] " + joinPoint.getSignature());
+        logger.info("[START] Iniciando chamada [{}.{}] com argumentos [{}}] ",
+                joinPoint.getSignature().getDeclaringType().getSimpleName(), joinPoint.getSignature().getName(), joinPoint.getArgs());
 
         Object obj = joinPoint.proceed();
+        SystemUser user = securityService.getAuthenticatedUser();
 
-        System.out.println("Depois [TESTE] " + joinPoint.getSignature());
+        logger.info("[AUDITORIA] Responsável pela alteração [{}] ROLES: [{}]", user.getLogin(), user.getRoles());
+        logger.info("[END] Chamada [{}.{}] finalizado!", joinPoint.getSignature().getDeclaringType().getSimpleName(), joinPoint.getSignature().getName());
         return obj;
     }
 
