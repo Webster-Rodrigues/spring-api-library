b10a82c5ee15db7124f73a647bf9881affd423ad
Webster Diogenes <web.driw12@gmail.com>
2025-09-24 20:24:36 -0300

    updates the delete logging aspect

--- src/main/java/io/github/websterrodrigues/libraryapi/aspects/LoggingAspect.java
+++ src/main/java/io/github/websterrodrigues/libraryapi/aspects/LoggingAspect.java
@@ -2,9 +2,10 @@ package io.github.websterrodrigues.libraryapi.aspects;
 
 import io.github.websterrodrigues.libraryapi.model.SystemUser;
 import io.github.websterrodrigues.libraryapi.security.SecurityService;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -71,7 +72,6 @@ public class LoggingAspect {
 
     @Around("updateEntityMethods()")
     public Object logAroundUpdate(ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 
         logger.debug("[START] Iniciando chamada [{}.{}]", joinPoint.getSignature().getDeclaringType().getSimpleName(), joinPoint.getSignature().getName());
         logger.info("[METHOD] {} | Atualizando {} ID {} no repository", joinPoint.getSignature().getName(), joinPoint.getArgs()[0].getClass().getSimpleName(),joinPoint.getArgs()[0].getClass().getMethod("getId").invoke(joinPoint.getArgs()[0]));
@@ -88,16 +88,15 @@ public class LoggingAspect {
 
     @Around("deleteEntityMethods()")
     public Object logAroundDelete(ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 
         logger.debug("[START] Iniciando chamada [{}.{}]", joinPoint.getSignature().getDeclaringType().getSimpleName(), joinPoint.getSignature().getName());
-        logger.info("[METHOD] {} | Deletando {} ID {} no repository", joinPoint.getSignature().getName(), signature.getReturnType().getSimpleName(), joinPoint.getArgs());
+        logger.info("[METHOD] {} | Deletando {} ID {} no repository", joinPoint.getSignature().getName(), joinPoint.getSignature().getDeclaringType().getSimpleName().replace("Service",""), joinPoint.getArgs()[0]);
 
         Object obj = joinPoint.proceed();
         SystemUser user = securityService.getAuthenticatedUser();
 
-        logger.info("[AUDIT] Responsável pela alteração [{}] ROLES: [{}]", user.getLogin(), user.getRoles());
-        logger.info("[RETURN] {} ID: {} deletado com sucesso!", obj.getClass().getSimpleName(),joinPoint.getArgs());
+        logger.info("[AUDIT] Responsável pela deleção [{}] ROLES: [{}]", user.getLogin(), user.getRoles());
+        logger.info("[RETURN] {} ID: {} deletado com sucesso!", joinPoint.getSignature().getDeclaringType().getSimpleName().replace("Service",""), joinPoint.getArgs()[0]);
         logger.debug("[END] Chamada [{}.{}] finalizada!", joinPoint.getSignature().getDeclaringType().getSimpleName(), joinPoint.getSignature().getName());
         return obj;
     }
